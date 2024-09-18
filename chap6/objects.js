5d30b30ff79d87a115c0779ec1468cf4b99c4993
lgretzinger <lgretzinger@sait.ucla.edu>
2024-09-18 14:41:06 -0400

    sdpoi

--- chap6/objects.js
+++ chap6/objects.js
@@ -1,4 +1,4 @@
-// Ex. 1 - Vector Type
+// Ex. 1 - A Vector Type
 class Vec {
     x;
     y;
@@ -13,7 +13,7 @@ class Vec {
     }
 
     plus(v2){
-        new Vec(this.x+v2.x, this.y+v2.y);
+        return new Vec(this.x+v2.x, this.y+v2.y);
     }
 
     minus(v2){ 
@@ -25,49 +25,118 @@ class Vec {
     }
 }
 
+// Ex. 2 - Groups
 class Group {
-    guys = {};
+    constructor() {
+        this.guys = {};
+    }
 
     add(x) {
-        if (!this.has(x))
-            this.guys = this.guys + [x];
-        return x;
+        if (!this.has(x)) {
+            this.guys[x] = true;
+        }
+        return false
     }
 
     has(x){
-        return this.guys[x]
-        // for (const i in this.guys) {
-        //     if (i === x)
-        //         return true
-        // }
-        // return false;
+        return !!(this.guys[x])
     }
 
     delete(x) {
         if(this.has(x)){
-            delete this.x
+            delete this.guys[x]
+            return `${x} deleted`
         } else {
-            return false
+            return `${x} not present`
         }
     }
+
+    static from(iter) {
+        let g = new Group()
+        for (const i of iter) {
+            g.add(i)
+        }
+        // for (const i in iter) {
+        //     g.add(iter[i])
+        // }
+        return g
+    }
+
+    toString() {
+        return Object.keys(this.guys).join(', ')
+    }
+
+    // Ex. 3 - GroupIterator
+    [Symbol.iterator]() {
+        let keys = Object.keys(this.guys)
+        let index = 0;
+
+        return {
+            next: () => {
+                if (index >= keys.length) {
+                    return {done: true}
+                } else {
+                    return {value: keys[index++], done: false}
+                }
+                let value = this.guys[keys[index]]
+                index++
+                return {value, done: false}
+            }
+        }
+      };
 }
 
 function main(){
     // Vector
-    // v = new Vec(3,4)
-    // v2 = new Vec(4,5)
-    // console.log(v.length)
-    // console.log(v.plus(v2))
-    // console.log(v.minus(v2))
-    // console.log(new Vec(1, 2).plus(new Vec(2, 3))); // → Vec{x: 3, y: 5}
-    // console.log(new Vec(1, 2).minus(new Vec(2, 3))); // → Vec{x: -1, y: -1}
-    // console.log(new Vec(3, 4).length);
+    let v = new Vec(3,4)
+    let v2 = new Vec(4,5)
+    console.log(v.length)
+    console.log(v.plus(v2))
+    console.log(v.minus(v2))
+    console.log(new Vec(1, 2).plus(new Vec(2, 3))); // → Vec{x: 3, y: 5}
+    console.log(new Vec(1, 2).minus(new Vec(2, 3))); // → Vec{x: -1, y: -1}
+    console.log(new Vec(3, 4).length);
 
     // Group
     g = new Group()
     g.add(4)
     g.add(6)
+    g.add(8)
+    g.add(10)
     console.log('ff',g)
+    console.log(g.delete(7))
+    console.log(g.delete(4))
+    console.log('fg',g)
+    console.log(g.has(6))
+    console.log('fg',g.toString())
+    console.log('-----------------')
+    
+    let group = Group.from([10, 20]);
+    console.log(group.has(10)); // → true
+    console.log(group.has(30)); // → false
+    group.add(10);
+    group.delete(10);
+    console.log(group.has(10)); // → false
+    console.log('-----------------')
+    
+    // Testing Group
+    group = Group.from([10, 20, 30]);
+    console.log(group.has(10)); // true
+    console.log(group.has(40)); // false
+    group.add(40);
+    console.log(group.toString()); // "10, 20, 30, 40"
+    group.delete(20);
+    console.log(group.toString()); // "10, 30, 40"
+    
+    // Testing Group iteration
+    for (let value of group) {
+        console.log(value);  // Iterates through "10", "30", "40"
+    }
+    console.log('-----------------')
+    
+    for (let i of Group.from(['a','b','c'])) {
+        console.log(i)
+    }
 }
 
 main()
